f6157ac4254353e91f8eb5e95b26592d80659b3f
Ozoda9 <106987660+Ozoda9@users.noreply.github.com>
2022-11-02 23:17:54 -0500

    trycloudAutomation project created, important files added

---
+++ src/test/java/com/trycloud/library/runners/CukesRunner.java
@@ -0,0 +1,19 @@
+package com.trycloud.library.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com/trycloud/library/step_definitions",
+        dryRun = false,
+        tags = "@"
+
+)
+
+public class CukesRunner {
+
+
+}
